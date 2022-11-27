67d8ccb8ed8ad2de5390085a7e614d0037be0cb3
shevi <vhyannisa04@gmail.com>
2022-11-27 15:37:49 +0700

    Update about.php

--- pages/about.php
+++ pages/about.php
@@ -10,15 +10,15 @@
     }
 </style>
 <body>
-    <h1 style="text-align:center;font-family: 'Lucida Handwriting', sans-serif;font: size 5rem">STYLE SHOP</h1>
+    <h1 style="text-align:font-family: 'Lucida Handwriting', sans-serif;font: size 5rem">STYLE SHOP</h1>
     <div class="picture"><img src="picture/pic.jpg" alt=""></div>
     <br>
     <br>
     <h2 style="text-align:center;font-family: 'Brush Script MT',sans-serif;font:size 5rem">Our Brand</h2>
-    <h3 style="text-align:center;font-family: 'Copperplate, Papyrus,fantasy',sans-serif;font:size 5rem"> style shop menawarkan berbagai macam pakaian yang sesuai dengan selera gaya wanita mana pun. Pakaian dan asesoris kami dikuratori dengan hati-hati untuk menyediakan mode terbaru kepada pelanggan kami. Untuk menjaga gaya pelanggan kami, kami memposting pendatang baru setiap hari, serta menawarkan pilihan stylist untuk membantu pembeli yang ragu-ragu. Style shop adalah tempat terbaik fashionista untuk membuat lemari pakaian yang sempurna.
+    <h3 style="text-align:center;font-family: 'Copperplate, Papyrus,fantasy',sans-serif;font:size 5rem"> <p>style shop menawarkan berbagai macam pakaian yang sesuai dengan selera gaya wanita mana pun. Pakaian dan asesoris kami dikuratori dengan hati-hati untuk menyediakan mode terbaru kepada pelanggan kami. Untuk menjaga gaya pelanggan kami, kami memposting pendatang baru setiap hari, serta menawarkan pilihan stylist untuk membantu pembeli yang ragu-ragu. Style shop adalah tempat terbaik fashionista untuk membuat lemari pakaian yang sempurna.</p>
 </h3>
-<h3> Selain membantu Anda tampil terbaik, style shop berusaha membuat setiap pembelian menjadi pengalaman yang positif. Prioritas utama kami adalah layanan pelanggan yang sangat baik, pemrosesan pesanan yang sangat cepat, waktu pengiriman yang sangat cepat, dan kebijakan pengembalian yang tidak merepotkan. Kami menghargai umpan balik Anda, baik positif maupun konstruktif, dan kami terus berupaya meningkatkan pengalaman Anda.</p>
+<h3> <p> Selain membantu Anda tampil terbaik, style shop berusaha membuat setiap pembelian menjadi pengalaman yang positif. Prioritas utama kami adalah layanan pelanggan yang sangat baik, pemrosesan pesanan yang sangat cepat, waktu pengiriman yang sangat cepat, dan kebijakan pengembalian yang tidak merepotkan. Kami menghargai umpan balik Anda, baik positif maupun konstruktif, dan kami terus berupaya meningkatkan pengalaman Anda.</p>
 
-<p> Jika Anda adalah pengunjung pertama kali atau pelanggan lama, kami harap Anda akan senang dengan setiap aspek pengalaman belanja Butik Online Impressions Anda. </p></h3>
+<p> Jika Anda adalah pengunjung pertama kali atau pelanggan lama, kami harap Anda akan senang dengan setiap aspek pengalaman belanja Butik Online Impressions Anda. </p> </h3>
 </body>
 </html>
