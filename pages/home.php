ee7ed18bfedc527c75f2d65a186725b367c09e85
mayamanis <dewimaya2403@gmail.com>
2022-11-27 00:28:23 -0800

    gvghgg
    njnj

--- pages/home.php
+++ pages/home.php
@@ -1,2 +1,16 @@
-<h3>INI HOME</h3>
-<h1>New Arrival</h1>
+  <style>
+    body{
+        background-color:#ffe2c5;
+        
+    }
+    .tengah{
+        display:flex;
+        justify-content:center;
+        
+    }
+  
+</style>
+<div class="tengah"><img src="img/k.jpg" alt=""></div>
+<h2 style="text-align:center;font-family: 'font-family: Arial, Helvetica, sans-serif;'"> boutique</h2>
+<h1 style="text-align:center;font-family: 'Monesstry Free', sans-serif;font-size:5rem">Your Style Is Our Priority</h1>    </div>
+
