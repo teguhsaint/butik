78b3babde7a84d6eb941d355c11fda07ae9f410f
Teguh Saint <75825300+teguhsaint@users.noreply.github.com>
2022-11-28 08:08:23 +0700

    animate

--- pages/home.php
+++ pages/home.php
@@ -1,8 +1,13 @@
 <style>
+    body {
+        background-color: white;
+    }
+
     .container {
         padding: 0;
         width: 100%;
         padding-top: 3px;
+
     }
 
     .width-80 {
@@ -99,40 +104,74 @@
     }
 
     .card_new {
+        cursor: pointer;
+
         border: 1px solid #e7e7e7;
 
         border-radius: 4px;
         overflow: hidden;
-        padding: 0.5rem;
+
+        height: 400px;
+        background-size: cover;
+        background-position: center;
     }
 
-    .card_new img {
+    .card-overlay {
         cursor: pointer;
-        width: 100%;
+        border-radius: 20px;
+        display: block;
+        height: 120px;
+
+        position: relative;
+        background-color: #ffffff;
+        bottom: -300px;
+        left: 0px;
+        padding-left: 10px;
+        padding-top: 2px;
+        color: black;
+
+
         transition-duration: 0.3s;
     }
 
-    .card_new:hover img {
-        transform: scale(1.1);
+    .card_new:hover .card-overlay {
+        border-radius: 0px;
+        bottom: 0px;
+        height: 500px;
+        background-color: #222222ff;
+        color: white;
 
     }
+
 
     .p-0 {
         padding: 0rem;
     }
 
-    .footers {
-        margin-top: 20px;
-        min-height: 300px;
-        width: 100%;
-        background-color: #1f1f1f;
+    .card_new button {
+        cursor: pointer;
+        padding: 15px 36px;
+        display: block;
+        border-radius: 40px;
+        border: 1px solid white;
+
+        background-color: #222;
+        color: white;
+    }
+
+    .card_new button:hover {
+
+        border: 1px solid white;
+
+        background-color: #ff3838;
+        color: #fff;
     }
 </style>
 <div class="container_banner">
 
     <div class="banner">
         <div class="box_text">
-            <h1 class="mb-0">Yout Style Is Our Priority</h1>
+            <h1 class="mb-0">Your Style Is Our Priority</h1>
             <h3 class="mt-0">Sub sambutan untuk calon cust</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
                 ac ante dapibus, lobortis ipsum a, ullamcorper lorem. Donec et ligula ut ex egestas vehicula a non mi.
@@ -146,26 +185,76 @@
     </div>
 
 </div>
+<div class="container">
+    <div class="konten width-80">
+        <h1 class="mt-0">New Arivals</h1>
 
-<div class="konten width-80">
-    <h1 class="mt-0">New Arival</h1>
+        <div class="konten_cards">
+            <div class="card_new" style="background-image:url('img/img_home/1.webp');">
+                <div class="card-overlay">
+                    <h4 class="mb-0">Two Tone Navy & Fuchsia</h4>
+                    <h2 style="color:#ff3838; " class="mt-0">Rp.150,000</h2>
+                    <p class="mb-0">Shirt dress warna navy-fuchsia ini memakai material bahan katun yang halus dan nyaman dipakai.</p>
+                    <p class="mt-0">Detail Bahan dan Ukuran :</p>
+                    <ul>
+                        <li> Bahan : Katun</li>
+                        <li> Lingkar Dada : 110 cm</li>
+                        <li> Lingkar Lengan : 50 cm</li>
+                        <li> Panjang Lengan : 51 cm</li>
+                        <li> Panjang Blus : 86 cm</li>
+                    </ul>
+                    <center><button>BUY NOW</button></center>
+                </div>
+            </div>
+            <div class="card_new" style="background-image:url('img/img_home/2.webp');">
+                <div class="card-overlay">
+                    <h4 class="mb-0">Two Tone Navy & Fuchsia</h4>
+                    <h2 style="color:#ff3838; " class="mt-0">Rp.150,000</h2>
+                    <p class="mb-0">Shirt dress warna navy-fuchsia ini memakai material bahan katun yang halus dan nyaman dipakai.</p>
+                    <p class="mt-0">Detail Bahan dan Ukuran :</p>
+                    <ul>
+                        <li> Bahan : Katun</li>
+                        <li> Lingkar Dada : 110 cm</li>
+                        <li> Lingkar Lengan : 50 cm</li>
+                        <li> Panjang Lengan : 51 cm</li>
+                        <li> Panjang Blus : 86 cm</li>
+                    </ul>
+                    <center><button>BUY NOW</button></center>
+                </div>
+            </div>
+            <div class="card_new" style="background-image:url('img/img_home/3.webp');">
+                <div class="card-overlay">
+                    <h4 class="mb-0">Two Tone Navy & Fuchsia</h4>
+                    <h2 style="color:#ff3838; " class="mt-0">Rp.150,000</h2>
+                    <p class="mb-0">Shirt dress warna navy-fuchsia ini memakai material bahan katun yang halus dan nyaman dipakai.</p>
+                    <p class="mt-0">Detail Bahan dan Ukuran :</p>
+                    <ul>
+                        <li> Bahan : Katun</li>
+                        <li> Lingkar Dada : 110 cm</li>
+                        <li> Lingkar Lengan : 50 cm</li>
+                        <li> Panjang Lengan : 51 cm</li>
+                        <li> Panjang Blus : 86 cm</li>
+                    </ul>
+                    <center><button>BUY NOW</button></center>
+                </div>
+            </div>
+            <div class="card_new" style="background-image:url('img/img_home/4.webp');">
+                <div class="card-overlay">
+                    <h4 class="mb-0">Two Tone Navy & Fuchsia</h4>
+                    <h2 style="color:#ff3838; " class="mt-0">Rp.150,000</h2>
+                    <p class="mb-0">Shirt dress warna navy-fuchsia ini memakai material bahan katun yang halus dan nyaman dipakai.</p>
+                    <p class="mt-0">Detail Bahan dan Ukuran :</p>
+                    <ul>
+                        <li> Bahan : Katun</li>
+                        <li> Lingkar Dada : 110 cm</li>
+                        <li> Lingkar Lengan : 50 cm</li>
+                        <li> Panjang Lengan : 51 cm</li>
+                        <li> Panjang Blus : 86 cm</li>
+                    </ul>
+                    <center><button>BUY NOW</button></center>
+                </div>
+            </div>
 
-    <div class="konten_cards">
-        <div class="card_new">
-            <img src="img/img_home/1.webp" alt="">
-        </div>
-        <div class="card_new">
-            <img src="img/img_home/2.webp" alt="">
-        </div>
-        <div class="card_new">
-            <img src="img/img_home/3.webp" alt="">
-        </div>
-        <div class="card_new">
-            <img src="img/img_home/4.webp" alt="">
         </div>
     </div>
-</div>
-
-<div class="footers">
-
 </div>
