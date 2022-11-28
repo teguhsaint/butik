7a7715763ce7f8fb3281d811ead76cb89e95e294
shevi <vhyannisa04@gmail.com>
2022-11-28 14:59:03 +0700

    v

--- pages/about.php
+++ pages/about.php
@@ -1,11 +1,16 @@
 <style>
    body{
-    background-color:#ffe2c5;
+    background-color:white;
    }
    .picture{
+        display:flex; 
+        justify-content:center; 
+
+        
+    }
+    .picture{
         display:flex;
         justify-content:center;
-        
 
     }
 </style>
@@ -15,10 +20,18 @@
     <br>
     <br>
     <h2 style="text-align:center;font-family: 'Brush Script MT',sans-serif;font:size 5rem">Our Brand</h2>
-    <h3 style="text-align:center;font-family: 'Copperplate, Papyrus,fantasy',sans-serif;font:size 5rem"> <p>style shop menawarkan berbagai macam pakaian yang sesuai dengan selera gaya wanita mana pun. Pakaian dan asesoris kami dikuratori dengan hati-hati untuk menyediakan mode terbaru kepada pelanggan kami. Untuk menjaga gaya pelanggan kami, kami memposting pendatang baru setiap hari, serta menawarkan pilihan stylist untuk membantu pembeli yang ragu-ragu. Style shop adalah tempat terbaik fashionista untuk membuat lemari pakaian yang sempurna.</p>
-</h3>
-<h3> <p> Selain membantu Anda tampil terbaik, style shop berusaha membuat setiap pembelian menjadi pengalaman yang positif. Prioritas utama kami adalah layanan pelanggan yang sangat baik, pemrosesan pesanan yang sangat cepat, waktu pengiriman yang sangat cepat, dan kebijakan pengembalian yang tidak merepotkan. Kami menghargai umpan balik Anda, baik positif maupun konstruktif, dan kami terus berupaya meningkatkan pengalaman Anda.</p>
-
-<p> Jika Anda adalah pengunjung pertama kali atau pelanggan lama, kami harap Anda akan senang dengan setiap aspek pengalaman belanja Butik Online Impressions Anda. </p> </h3>
+    <p>style shop menawarkan berbagai macam pakaian yang sesuai dengan selera gaya remaja saat ini. Pakaian dan asesoris kami dikuratori dengan hati-hati untuk menyediakan mode terbaru kepada pelanggan kami. Untuk menjaga gaya pelanggan kami, kami memposting pendatang baru setiap hari, serta menawarkan pilihan stylist untuk membantu pembeli yang ragu-ragu. Style shop adalah tempat terbaik fashionista untuk membuat lemari pakaian yang sempurna.</p>
+    <p> Selain membantu anda tampil terbaik, style shop berusaha membuat setiap pembelian menjadi pengalaman yang positif. Prioritas utama kami adalah layanan pelanggan yang sangat baik, pemrosesan pesanan yang sangat cepat, waktu pengiriman yang sangat cepat, dan kebijakan pengembalian yang tidak merepotkan. Kami menghargai umpan balik anda, baik positif maupun konstruktif, dan kami terus berupaya meningkatkan pengalaman berbelanja anda.</p>
+    <p> Jika anda adalah pengunjung pertama kali atau pelanggan lama, kami harap anda akan senang dengan setiap aspek pengalaman berbelanja anda di stayle shop kami.</p> 
+    <br>
+    <br>
+    <div class="picture"><img src="img/pic2.jpg" alt=""></div>
+    <br>
+    <br>
+     <h2 style="text-align:center;font-family: 'Brush Script MT',sans-serif;font:size 5rem">Owner Shop</h2>
+     <p>Style Shop adalah butik yang dirintis oleh sekelompok mahasiswa yang ingin memulai bisnis online. Dengan tekad yang kuat, akhirnya kami dapat mewujudkan cita-cita bersama tersebut dengan meluncurkan situs web stayle shop.</p>
+     <p>Kami meluncurkan situs web ini pada tahun 2022 dengan hasrat untuk menciptakan pengalaman berbelanja yang unik. Kami memiliki keinginan kuat untuk membuat situs di mana remaja dari segala usia, bentuk, ukuran, dan anggaran dapat menemukan pakaian impian mereka. Bukan hanya pakainan yang sempurna, tetapi juga aksesoris lain yang dibutuhkan untuk menjadikannya pakaian impian anda. Kami ingin menciptakan mode yang dapat diperoleh semua orang, karena kami percaya bahwa pakaian sangat memengaruhi perasaan mereka. Setiap orang berhak untuk merasa tampil menarik, dan itulah yang ingin kami ciptakan.</p>
+     <p>Saat ini style shop terdiri dari 14 mahasiswa yang bekerjasama mengembangkan style shop diluar kesibukan kuliah. Kami sangat bersyukur atas apa yang dapat kami lakukan setiap hari, dan tim kami yang sudah seperti keluarga sendiri adalah yang membuat perusahaan kami sukses seperti sekarang ini.</p>
+     <p>Terima kasih telah meluangkan waktu untuk mengenal kami sedikit. Anda berada di sini, di situs kami, lebih berarti bagi kami daripada yang pernah anda ketahui. Apakah Anda pelanggan pertama kali, pelanggan setia jangka panjang, atau sekadar berbelanja di jendela, saya sangat senang anda ada di sini. Kami harap anda memberi kami kesempatan untuk mengesankan anda.</p>
 </body>
 </html>
